e97f1a7e3c9f14d689dd32a69c79d905652a9e14
Philipp Alber <philipp_j.alber@yahoo.com>
2019-03-14 09:52:30 +0100

    fixed navbar and dropdown; hover effects for social media links

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,14 @@
-// init navbar
+// initialising components
 document.addEventListener('DOMContentLoaded', () => {
+    // init sidenav
     let elems = document.querySelectorAll('.sidenav');
-    let instances = M.Sidenav.init(elems, {
+    M.Sidenav.init(elems, {
         edge: "right"
+    });
+
+    // init navbar dropdown menu
+    let dropdown = document.querySelector(".dropdown-trigger");
+    M.Dropdown.init(dropdown, {
+        hover: false
     });
 });
